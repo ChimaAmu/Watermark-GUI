565bfe2602989980c6a63564e5f27a40ed668171
salad <chimaamu@proton.me>
2025-11-20 16:04:04 +0000

    Change gwenview to imagemagick for consistency, improve zenity dialogues, add further comments

--- wm.sh
+++ wm.sh
@@ -1,44 +1,56 @@
 #!/bin/bash
-# wm--Adds specified text as a watermark on the input image,
-#     saving the output as image+wm
+# Adds specified text as a watermark on the input image,
+# saving the output as image+wm
 
-wmfile="/tmp/watermark.$$.png"
+tmpfile="/tmp/watermark.$$.png"
 fontsize="44"
 
+
 file=$(zenity --title="Select file to watermark" \
-              --file-selection) >/dev/null 2>&1
+              --file-selection)
+
+# Check that a file was selected
 case $? in
      0) echo "\"$file\" selected.";;
      1) echo "No file selected."; exit 1 ;;
     -1) echo "An unexpected error has occurred."; exit 1 ;;
 esac
 
+# File must be a png, jp(e)g, or webp
+validtypes="(.png|.jpe?g|.webp)"
+! [[ "$file" =~ $validtypes ]] && \
+    zenity --error --text="Unsupported filetype, must be png, jpg, or webp" && \
+    exit 1
+
 watermark=$(zenity --title="What would you like your watermark to be?"\
-                   --entry) >/dev/null 2>&1
+                   --entry)
 
-trap '$(which rm) -f $wmfile' 0 1 15
+# Allow user to cancel if they change their mind
+if [ "$?" = "1" ] ; then exit 1 ; fi
 
-# To start, get the dimensions of the image
+# Delete temporary file after program closes
+trap '$(which rm) -f $tmpfile' 0 1 15
 
 dimensions="$(identify -format "%G" "$file")"
 
-# Let's create the temporary watermark overlay
-
+# Create temporary watermark overlay
 convert -size "$dimensions" xc:none -pointsize $fontsize -gravity south \
     -draw "fill black text 1,1 '$watermark' text 0,0 '$watermark' fill white text 2,2 '$watermark'" \
-    "$wmfile"
+    "$tmpfile"
 
-# Now let's composite the overlay and the original file
+# Composite overlay and original file
 suffix="$(echo "$file" | rev | cut -d. -f1 | rev)"
 prefix="$(echo "$file" | rev | cut -d. -f2- | rev)"
 
 newfilename="$prefix+wm.$suffix"
-composite -dissolve 75% -gravity south $wmfile "$file" "$newfilename"
+composite -dissolve 75% -gravity south $tmpfile "$file" "$newfilename"
 
 if [ -r "$newfilename" ] ; then
     echo "Created new watermarked image file $newfilename"
+    # "OK" selection exit code is 0
+    # If user enters "OK", then show file
     if zenity --question --icon="$newfilename" --text="Would you like to see the newly watermarked file?" ; then
-        gwenview "$newfilename"
+        magick display "$newfilename"
     fi
 else
     echo "An unexpected error occurred"
