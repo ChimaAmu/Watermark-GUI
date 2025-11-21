bf4fd9af7aec0f4d1f17e1abbf0bc76141417d64
salad <chimaamu@proton.me>
2025-11-21 22:25:25 +0000

    Add support for watermarking multiple files at once

--- wm.sh
+++ wm.sh
@@ -6,55 +6,65 @@ tmpfile="/tmp/watermark.$$.png"
 fontsize="44"
 
 
-file=$(zenity --title="Select file to watermark" \
-              --file-selection)
+file=$(zenity --title="Select file(s) to watermark" \
+              --file-selection \
+              --multiple)
 
-# Check that a file was selected
-case $? in
-     0) echo "\"$file\" selected.";;
-     1) echo "No file selected."; exit 1 ;;
-    -1) echo "An unexpected error has occurred."; exit 1 ;;
-esac
+# Find how many files were selected by converting the default separator ('|')
+# into newlines ('\n'), and then counting how many lines there are with wc -l
+filenum=$(echo "$file" | tr \| \\n | wc -l)
+# List of each file separated by newlines
+files=$(echo "$file" | tr \| \\n)
+
+# Make sure user knows they have selected more than one file
+if [ "$filenum" -gt 1 ] ; then
+    zenity --question --text="Do you want to apply a watermark to more than one file?"
+    # Allow user to cancel if they change their mind
+    if [ "$?" = "1" ] ; then exit 1 ; fi
+fi
 
 # File must be a png, jp(e)g, or webp
 validtypes="(.png|.jpe?g|.webp)"
-! [[ "$file" =~ $validtypes ]] && \
-    zenity --error --text="Unsupported filetype, must be png, jpg, or webp" && \
-    exit 1
+for file in $files; do
+    ! [[ "$file" =~ $validtypes ]] && \
+        zenity --error --text="Unsupported filetype, must be png, jpg, or webp" && \
+        exit 1
+done
 
 watermark=$(zenity --title="What would you like your watermark to be?"\
                    --entry)
-
-# Allow user to cancel if they change their mind
 if [ "$?" = "1" ] ; then exit 1 ; fi
 
-# Delete temporary file after program closes
-trap '$(which rm) -f $tmpfile' 0 1 15
 
-dimensions="$(identify -format "%G" "$file")"
+for file in $files; do
+    # Delete temporary file after program closes
+    trap '$(which rm) -f $tmpfile' 0 1 15
 
-# Create temporary watermark overlay
-convert -size "$dimensions" xc:none -pointsize $fontsize -gravity south \
-    -draw "fill black text 1,1 '$watermark' text 0,0 '$watermark' fill white text 2,2 '$watermark'" \
-    "$tmpfile"
+    dimensions="$(identify -format "%G" "$file")"
 
-# Composite overlay and original file
-suffix="$(echo "$file" | rev | cut -d. -f1 | rev)"
-prefix="$(echo "$file" | rev | cut -d. -f2- | rev)"
+    # Create temporary watermark overlay
+    convert -size "$dimensions" xc:none -pointsize $fontsize -gravity south \
+        -draw "fill black text 1,1 '$watermark' text 0,0 '$watermark' fill white text 2,2 '$watermark'" \
+        "$tmpfile"
 
-newfilename="$prefix+wm.$suffix"
-composite -dissolve 75% -gravity south $tmpfile "$file" "$newfilename"
+    # Composite overlay and original file
+    suffix="$(echo "$file" | rev | cut -d. -f1 | rev)"
+    prefix="$(echo "$file" | rev | cut -d. -f2- | rev)"
 
-if [ -r "$newfilename" ] ; then
-    echo "Created new watermarked image file $newfilename"
-    # "OK" selection exit code is 0
-    # If user enters "OK", then show file
-    if zenity --question --icon="$newfilename" --text="Would you like to see the newly watermarked file?" ; then
-        magick display "$newfilename"
+    newfilename="$prefix+wm.$suffix"
+    composite -dissolve 75% -gravity south $tmpfile "$file" "$newfilename"
+
+    if [ -r "$newfilename" ] ; then
+        echo "Created new watermarked image file $newfilename"
+        # "OK" selection exit code is 0
+        # If user enters "OK", then show file
+        if zenity --question --icon="$newfilename" --text="Would you like to see the newly watermarked file?" ; then
+            magick display "$newfilename"
+        fi
+    else
+        echo "An unexpected error occurred"
+        exit 1
     fi
-else
-    echo "An unexpected error occurred"
-    exit 1
-fi
+done
 
 exit 0
